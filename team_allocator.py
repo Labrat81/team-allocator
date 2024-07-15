a1088e6cb394cb7ddb114a3914340fda552855b5
Wayne Senior <wayne_senior@hotmail.com>
2024-07-15 21:54:40 +0100

    change code to assign both teams before printing

--- team_allocator.py
+++ team_allocator.py
@@ -15,17 +15,14 @@ while True:
     random.shuffle(players)
 
     team1 = players[:len(players)//2]
+    team2 = players[len(players)//2:]
 
     print('\nTeam 1 captain: ' + random.choice(team1))
-
     print('\nTeam 1: ')
     for player in team1:
         print(player)
 
-    team2 = players[len(players)//2:]
-
     print('\nTeam 2 captain: ' + random.choice(team2))
-
     print('\nTeam 2: ')
     for player in team2:
         print(player)
